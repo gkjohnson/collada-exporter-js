a102ed20e48801a5186b876b803a619b42b2cf1e
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-06-22 13:07:03 -0700

    Example content update

--- example.js
+++ example.js
@@ -5,6 +5,7 @@ var saveData = ( function () {
 	document.body.appendChild( a );
 	a.style = 'display: none';
 	return function ( data, fileName ) {
+
 		var blob = new Blob( [ data ], { type: 'octet/stream' } ),
 			url = window.URL.createObjectURL( blob );
 		a.href = url;
@@ -32,18 +33,45 @@ var parseAndDownload = obj => {
 	const zip = new JSZip();
 	zip.file( 'example.dae', data.data );
 	data.textures.forEach( tex => zip.file( `${ tex.name }.${ tex.ext }`, tex.data ) );
-	// saveData( zip.generateAsync( { type: 'uint8Array' } ), 'colladaexample.zip' );
+	// saveData( zip.generate( { type: 'uint8Array' } ), 'colladaexample.zip' );
 
 };
 
 // create the geometry
-var geometry = new THREE.SphereBufferGeometry( 5, 10, 10 );
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-var mesh = new THREE.Mesh( geometry, material );
-var obj = new THREE.Object3D();
+// var geometry = new THREE.SphereBufferGeometry( 5, 10, 10 );
+// var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+// var mesh = new THREE.Mesh( geometry, material );
+// var obj = new THREE.Object3D();
 
-obj.add( mesh );
-parseAndDownload( obj );
+// obj.add( mesh );
+// parseAndDownload( obj );
 
-// Load and process and externally loaded collada model
-// loader.load( 'elf.dae', res => parseAndDownload( res.scene ) );
+// Visualize the processed model
+customElements.define( 'model-viewer', ModelViewer );
+
+var el = document.querySelector( 'model-viewer' );
+loader.load( './testmodels/pump/pump.dae', res => {
+
+	// wait 1 second to make sure the textures have downloaded
+	setTimeout( () => {
+
+		var data = exp.parse( res.scene );
+		var daeurl = URL.createObjectURL( new Blob( [ data.data ] ) ) + '#.dae';
+		el.loadingManager.setURLModifier( url => {
+
+			const tex = Object
+				.keys( data.textures )
+				.filter( n => url.indexOf( data.textures[ n ].name ) !== - 1 )
+				.pop();
+
+			if ( ! tex ) return url;
+			else return URL.createObjectURL( new Blob( [ data.textures[ tex ].data ] ) );
+
+		} );
+
+		el.src = daeurl;
+		setTimeout( () => el._dirty = true, 1000 );
+
+	}, 1000 );
+
+} );
